4230389f2808b5b5ff647ef51df53b8860e601a0
B19040229谭永锋 <tan13621251388@gmail.com>
2021-10-25 13:10:52 +0800

    初始化项目

---
+++ src/main/java/cn/njupt/votingsystem/VotingSystemApplication.java
@@ -0,0 +1,13 @@
+package cn.njupt.votingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSystemApplication.class, args);
+    }
+
+}
